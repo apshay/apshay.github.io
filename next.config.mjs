1402bc4a289cca602968b1e555792e508918f6b6
Aiden Shay <aidenshay77@gmail.com>
2024-07-18 21:22:07 -0400

    Update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    images: {
+      loader: "akamai",
+      path: "",
+    },
+    assetPrefix: "./",
 };
 
 export default nextConfig;
